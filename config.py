59b5b51251c0bfb5d934ebe9ff61373074c8897e
ashvangoga <ashvangoga@gmail.com>
2025-10-23 19:33:58 -0400

    feat: Implement SCRUM-1 - Create User Database, add models, and mock data

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db') # Fallback to SQLite if URL not set
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
